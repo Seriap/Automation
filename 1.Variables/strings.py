20dde3aaf0a31b301eca4b4425363435d45d5b15
Seriap <seria000111@gmai.com>
2022-12-16 22:37:59 +0600

    Hi

---
+++ 1.Variables/strings.py
@@ -0,0 +1,12 @@
+message = 'Hello Hello world'
+
+first_name = input('What is your first name: ')
+last_name = input('What is your last name: ')
+
+full_name = first_name + ' ' + last_name
+print(full_name.upper())
+print(full_name.lower())
+print(len(full_name))
+
+count = message.count('Hello')
+print(count)
---
+++ 1.Variables/variable.py
@@ -0,0 +1,35 @@
+"""
+1. integer
+2. Float
+3. String
+4. Boolean
+"""
+"""
+first_name = 'Mr'
+last_name = "Superman "
+marks = 80
+tax = 10.5
+active_user = True
+#logged_status = False
+first_name = 'Mr'
+
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+"""
+
+first_name = input('What is your first name: ')
+last_name = input('What is your last name: ')
+age = input('What is your age: ')
+address = input('What is your address: ')
+full_name = first_name + ' ' + last_name
+print("Hello, " + full_name + " your age is " + age + ",and you live in " + address)
+
+
+# Hello, Mr. Super your age is 10, and you live in Dhaka
+
+
+
+
+
+
